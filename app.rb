e944bfd164f5e106e3cce16b8b69119e0f184b70
Oleh <emchuk86@gmail.com>
2019-01-16 17:50:52 +0200

    add visit erb

--- app.rb
+++ app.rb
@@ -12,8 +12,39 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.all
+end
 
 get '/' do
 	@barbers = Barber.order "created_at DESC"
 	erb :index
+end
+
+get '/visit' do
+
+  
+  erb :visit
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+post '/visit' do
+  # user_name, phone, date_time
+  @user_name = params[:user_name]
+  @phone = params[:phone]
+  @date_time = params[:date_time]
+  @barber = params[:barber]
+  @color = params[:color]
+
+  hh = {:user_name => 'Введите имя', 
+    :phone => 'Введите телефон', 
+    :date_time => 'Введите дату и время'}
+
+
+  erb "<h1>Спасибо, Вы записались!</h2>"
+
+
 end
