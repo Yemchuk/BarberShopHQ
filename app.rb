4339530ac2e9b4d8c7778030bdc3a7562da0e9ee
Oleh <emchuk86@gmail.com>
2019-01-21 20:56:49 +0200

    display error text

--- app.rb
+++ app.rb
@@ -38,9 +38,12 @@ end
 
 post '/visit' do
 	c = Client.new params[:client]
-	c.save
-
-  	erb "<h1>Спасибо, Вы записались!</h2>"
+	if c.save
+	  	erb "<h1>Спасибо, Вы записались!</h2>"
+	else
+		@error = c.errors.full_messages.first
+		erb :visit
+	end
 
 end
 
